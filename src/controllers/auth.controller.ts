e0df19ae59460347c08bc1a5c0d35f63d202b160
ngoctin-ondamic <ngoctincodeaholic@gmail.com>
2022-07-13 22:23:57 -0500

    m

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import {Request,Response} from 'express'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import User from '../models/user.model';
 import MyHelper from '../helper';
 import UserQueries from '../databases/user.query';
